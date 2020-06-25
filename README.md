da7773da36ad78e614e4a78693e90d635e4f2ef5
Imran-Subhan <isubhan@qa.com>
2020-06-25 11:38:56 +0100

    java repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# Imran
-# JAVA_B
---
+++ src/QA_Software1.java
@@ -0,0 +1,9 @@
+
+public class QA_Software1 {
+
+	public static void main(String[] args) {
+		String message = "Hello World!";
+		System.out.println(message);
+		
+	}
+}
